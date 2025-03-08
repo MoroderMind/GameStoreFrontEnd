8ead6ea5b462ba4fb04960c84d28e2204538627d
MoroderMind <moroderwow@gmail.com>
2025-03-08 19:02:53 +0100

    Added validation, by doing data annotations in the models, and adding validationmessage blazor inputs in the HTML

--- GameStoreFrontend/obj/Debug/net9.0/GameStoreFrontend.AssemblyInfo.cs
+++ GameStoreFrontend/obj/Debug/net9.0/GameStoreFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStoreFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b042065ed0edfea682b78f16fc42d784dd02a8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a9fb8d20a9aa6fb69355b2cde6929a0f4e51a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStoreFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStoreFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
